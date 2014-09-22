a51469354975587246e66ee0d0939df875fb019d
Hugo Penedones <hpenedones@gmail.com>
2014-09-22 01:43:24 +0100

    Added support to read structs. Started support for cells (incomplete)

--- init.lua
+++ init.lua
@@ -5,48 +5,40 @@ local mat = require 'matio.ffi'
 local matio = {}
 matio.ffi = mat
 
-local function mat_read_variable(file, name)
-   local var = mat.varRead(file, name);
-   if var == nil then
-      print('Could not find variable with name: ' .. name .. ' in file: ' .. ffi.string(mat.getFilename(file)))
-      mat.close(file);
-      return
-   end
+
+local tensor_types_mapper = {
+   [mat.C_CHAR]   = {constructor='CharTensor',  sizeof=1},
+   [mat.C_INT8]   = {constructor='CharTensor',  sizeof=1},
+   [mat.C_UINT8]  = {constructor='ByteTensor',  sizeof=1},
+   [mat.C_INT16]  = {constructor='ShortTensor', sizeof=2},
+   [mat.C_UINT16] = {constructor='ShortTensor', sizeof=2},
+   [mat.C_INT32]  = {constructor='IntTensor',   sizeof=4},
+   [mat.C_UINT32] = {constructor='IntTensor',   sizeof=4},
+   [mat.C_INT64]  = {constructor='LongTensor',  sizeof=8},
+   [mat.C_UINT64] = {constructor='LongTensor',  sizeof=8},
+   [mat.C_SINGLE] = {constructor='FloatTensor', sizeof=4},
+   [mat.C_DOUBLE] = {constructor='DoubleTensor',sizeof=8}
+}
+
+local function load_tensor(file, var)
    local out
    local sizeof
    -- type check
-   if var.class_type == mat.C_CHAR or var.class_type == mat.C_INT8 then
-      out = torch.CharTensor()
-      sizeof = 1
-   elseif var.class_type == mat.C_UINT8 then
-      out = torch.ByteTensor()
-      sizeof = 1
-   elseif var.class_type == mat.C_INT16 or var.class_type == mat.C_UINT16 then
-      out = torch.ShortTensor()
-      sizeof = 2
-   elseif var.class_type == mat.C_INT32 or var.class_type == mat.C_UINT32 then
-      out = torch.IntTensor()
-      sizeof = 4
-   elseif var.class_type == mat.C_INT64 or var.class_type == mat.C_UINT64 then
-      out = torch.LongTensor()
-      sizeof = 8
-   elseif var.class_type == mat.C_SINGLE then
-      out = torch.FloatTensor()
-      sizeof = 4
-   elseif var.class_type == mat.C_DOUBLE then
-      out = torch.DoubleTensor()
-      sizeof = 8
+   mapper = tensor_types_mapper[tonumber(var.class_type)]
+   if mapper then
+      out = torch[mapper.constructor]()
+      sizeof = mapper.sizeof   
    else
-      print('Unsupported type of variable, only matrices are supported, cells, structs, objects and functions are not supported for loading')
-      mat.close(file);
-      return
+      print('Unsupported type of tensor: ' .. var.class_type)
+      print('Only matrices are supported')
+      return nil
    end
 
    -- rank check
    if var.rank > 8 or var.rank < 1 then
       print('Rank of input matrix is invalid: ' .. var.rank)
-      mat.close(file);
-      return
+      
+      return nil
    end
    
    local sizes = {}
@@ -65,7 +57,7 @@ local function mat_read_variable(file, name)
    -- memcpy
    ffi.copy(out:data(), var.data, out:nElement() * sizeof);
    mat.varFree(var);
-   mat.close(file);
+   
    -- -- transpose, because matlab is column-major
    if out:dim() > 1 then
       for i=1,math.floor(out:dim()/2) do
@@ -73,6 +65,39 @@ local function mat_read_variable(file, name)
       end
    end
    return out
+end
+
+local function mat_read_variable(file, var)
+   print(var.class_type)   
+
+   if tensor_types_mapper[tonumber(var.class_type)] then
+      print('loading tensor')
+      return load_tensor(file, var)
+   end
+
+   if var.class_type == mat.C_CELL then
+      print('loading cell')
+      -- todo: finish support for cells (currently incorrect)
+      local cell = mat.varGetCell(var, 0)
+      return mat_read_variable(file, cell)
+   end
+
+   local out = {}
+ 
+   if var.class_type == mat.C_STRUCT then
+       print('loading struct')
+       n_fields = mat.varGetNumberOfFields(var)
+       field_names =  mat.varGetStructFieldnames(var)
+       for i=0,n_fields-1 do
+         field_name = ffi.string(field_names[i])
+         print(field_name)
+         field_value = mat.varGetStructFieldByIndex(var, i, 0)
+         out[field_name] = mat_read_variable(file, field_value)
+      end   
+   end 
+   
+   return out
+
 end
 
 --[[
@@ -107,7 +132,9 @@ function matio.load(filename, name)
       -- go over the file and get the names
       local var = mat.varReadNextInfo(file)
       while var ~= nil do
-         table.insert(names, ffi.string(var.name))
+         var_name_str = ffi.string(var.name)
+         table.insert(names, var_name_str)
+         print(var_name_str)
          var = mat.varReadNextInfo(file)
       end
    end
@@ -119,12 +146,15 @@ function matio.load(filename, name)
 
    local out = {}
    for i, varname in ipairs(names) do
-      local x = mat_read_variable(file, varname)
-      if not x then
-         return
+      local var = mat.varRead(file, varname);
+      local x = mat_read_variable(file, var)
+      if x == nil then
+         print('Could not find variable with name: ' .. name .. ' in file: ' .. ffi.string(mat.getFilename(file)))
       end
       out[varname] = x
    end
+
+   mat.close(file)
 
    -- conserve backward compatibility
    if #names == 1 and string_name then
